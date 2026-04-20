7df6988c77f5c98bcba059e699e96e6091884c43
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:02:15 +0400

    Task1:word0

--- src/word0.java
+++ src/word0.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
     public static void main(String[] args) {
         System.out.println(word0(new String[]{"a", "b", "a", "b"})); // {"a": 0, "b": 0}
         System.out.println(word0(new String[]{"a", "b", "a", "c", "b"})); // {"a": 0, "b": 0, "c": 0}
-        System.out.println(word0(new String[]{"c", "b", "a"}));
+        System.out.println(word0(new String[]{"c", "b", "a"})); //{"a": 0, "b": 0, "c": 0}
     }
 
     public static Map<String, Integer> word0(String[] strings) {
