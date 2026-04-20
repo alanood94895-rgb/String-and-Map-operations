857657d5724c67f128f61bb70d75dfead7910075
CompanyProfile <alanood94895@gmail.com>
2026-04-20 14:28:06 +0400

    Task8:allSwap

--- src/allSwap.java
+++ src/allSwap.java
@@ -4,17 +4,11 @@ import java.util.HashMap;
 public class allSwap {
 
     public static void main(String[] args) {
-        System.out.println(java.util.Arrays.toString(
-                allSwap(new String[]{"ab", "ac"})
-        ));
+        System.out.println((allSwap(new String[]{"ab", "ac"})));
 
-        System.out.println(java.util.Arrays.toString(
-                allSwap(new String[]{"ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"})
-        ));
+        System.out.println((allSwap(new String[]{"ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"})));
 
-        System.out.println(java.util.Arrays.toString(
-                allSwap(new String[]{"ax", "bx", "ay", "by", "ai", "aj", "bx", "by"})
-        ));
+        System.out.println((allSwap(new String[]{"ax", "bx", "ay", "by", "ai", "aj", "bx", "by"})));
     }
 
     public static String[] allSwap(String[] strings) {
@@ -31,7 +25,7 @@ public class allSwap {
                 strings[i] = strings[j];
                 strings[j] = temp;
 
-                // remove key so it can be used again correctly
+                // remove key
                 map.remove(key);
             } else {
                 map.put(key, i);
