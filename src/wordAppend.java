e4f07681f1f2f835d286fdf365f7e687ce64a006
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:59:06 +0400

    Task6:wordAppend

--- src/wordAppend.java
+++ src/wordAppend.java
@@ -5,7 +5,7 @@ public class wordAppend {
     public static void main(String[] args) {
         System.out.println(wordAppend(new String[]{"a", "b", "a"})); // "a"
         System.out.println(wordAppend(new String[]{"a", "b", "a", "c", "a", "d", "a"})); // "aa"
-        System.out.println(wordAppend(new String[]{"a", "", "a"}));
+        System.out.println(wordAppend(new String[]{"a", "", "a"})); // "a"
     }
 
     public static String wordAppend(String[] strings) {
