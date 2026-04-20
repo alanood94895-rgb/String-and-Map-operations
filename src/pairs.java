1fa863a2c5f458cb97844eb1b276b884bcbad778
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:20:19 +0400

    Task3:pairs

--- src/pairs.java
+++ src/pairs.java
@@ -5,7 +5,7 @@ public class pairs {
     public static void main (String[] args){
         System.out.println(pairs(new String[]{"code", "bug"}));
         System.out.println(pairs(new String[]{"man", "moon", "main"}));
-        System.out.println();
+        System.out.println(pairs(new String[]{"man", "moon", "good", "night"}));
     }
 
     public static Map<String, String> pairs (String[] String){
