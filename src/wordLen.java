f860d77f7c695427818bdb5b88f7d2d0643c8d9e
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:11:31 +0400

    Task2:wordLen

--- src/wordLen.java
+++ src/wordLen.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class wordLen {
     public class void main (String[] args){
-        System.out.println(wordLen (new String[]{"a", "bb", "a", "bb"}));
+        System.out.println(wordLen (new String[]{"a", "bb", "a", "bb"})); // {"bb": 2, "a": 1}
         System.out.println(wordLen(new String[]{"this", "and", "that", "and"}));
         System.out.println(wordLen(new String[]{"this", "and", "that", "and"}));
     }
