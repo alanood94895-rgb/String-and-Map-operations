c51a8638d671adf23dd7c53ad6c4bcc3b7879563
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:38:00 +0400

    Task5:firstChar

---
+++ src/firstChar.java
@@ -0,0 +1,29 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class firstChar {
+    public static void main(String[] args) {
+        System.out.println(firstChar(new String[]{"salt", "tea", "soda", "toast"}));
+        System.out.println(firstChar(new String[]{"aa", "bb", "cc", "aAA", "cCC", "d"}));
+        System.out.println(firstChar(new String[]{}));
+    }
+
+    public static Map<String, String> firstChar(String[] strings) {
+        Map<String, String> result = new HashMap<>();
+
+        for (String str : strings) {
+            String key = str.substring(0, 1);
+
+            if (result.containsKey(key)) {
+                result.put(key, result.get(key) + str);
+            } else {
+                result.put(key, str);
+            }
+        }
+
+        return result;
+    }
+}
+
+
+
