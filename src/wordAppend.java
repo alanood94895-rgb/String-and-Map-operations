94273960acaaabb2eed657225262f77b531de823
CompanyProfile <alanood94895@gmail.com>
2026-04-20 12:14:08 +0400

    Task7:wordAppend

--- src/wordAppend.java
+++ src/wordAppend.java
@@ -3,24 +3,27 @@ import java.util.HashMap;
 
 public class wordAppend {
     public static void main(String[] args) {
-        System.out.println(wordAppend(new String[]{"a", "b", "a"})); // "a"
-        System.out.println(wordAppend(new String[]{"a", "b", "a", "c", "a", "d", "a"})); // "aa"
-        System.out.println(wordAppend(new String[]{"a", "", "a"})); // "a"
+        System.out.println(wordAppend(new String[]{"a", "b", "a"}));
+        System.out.println(wordAppend(new String[]{"a", "b", "a", "c", "a", "d", "a"}));
+        System.out.println(wordAppend(new String[]{"a", "", "a"}));
     }
 
     public static String wordAppend(String[] strings) {
         Map<String, Integer> countMap = new HashMap<>();
-        StringBuilder result = new StringBuilder();
+        String result = "";
 
         for (String str : strings) {
-            int count = countMap.getOrDefault(str, 0) + 1;
-            countMap.put(str, count);
+            if (countMap.containsKey(str)) {
+                countMap.put(str, countMap.get(str) + 1);
+            } else {
+                countMap.put(str, 1);
+            }
 
-            if (count % 2 == 0) {
-                result.append(str);
+            if (countMap.get(str) % 2 == 0) {
+                result += str;
             }
         }
 
-        return result.toString();
+        return result;
     }
 }
