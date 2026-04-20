7bda591996c954c4372e9df0defe213634ef9bc3
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:26:22 +0400

    Task4:wordCount

--- src/wordCount.java
+++ src/wordCount.java
@@ -3,9 +3,10 @@ import java.util.HashMap;
 
 public class wordCount {
     public static void main(String[] args) {
-        System.out.println(wordCount(new String[]{"a", "b", "a", "c", "b"}));
-        System.out.println(wordCount(new String[]{"c", "b", "a"}));
-        System.out.println(wordCount(new String[]{"c", "c", "c", "c"}));
+        System.out.println(wordCount(new String[]{"a", "b", "a", "c", "b"})); // {"a": 2, "b": 2, "c": 1}
+        System.out.println(wordCount(new String[]{"c", "b", "a"}));  // {"a": 1, "b": 1, "c": 1}
+        System.out.println(wordCount(new String[]{"c", "c", "c", "c"}));  //
+
     }
 
     public static Map<String, Integer> wordCount(String[] strings) {
