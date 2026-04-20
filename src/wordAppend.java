6de3631114373e2e3af8247a79764c15c434f234
CompanyProfile <alanood94895@gmail.com>
2026-04-20 12:15:03 +0400

    Task7:wordAppend

--- src/wordAppend.java
+++ src/wordAppend.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 public class wordAppend {
     public static void main(String[] args) {
-        System.out.println(wordAppend(new String[]{"a", "b", "a"}));
+        System.out.println(wordAppend(new String[]{"a", "b", "a"})); // {"a": true, "b": true, "c": false}
         System.out.println(wordAppend(new String[]{"a", "b", "a", "c", "a", "d", "a"}));
         System.out.println(wordAppend(new String[]{"a", "", "a"}));
     }
