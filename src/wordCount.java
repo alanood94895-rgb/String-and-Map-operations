fb1f39f4484d78576a436ecc6594b207cbcf6a84
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:26:31 +0400

    Task4:wordCount

--- src/wordCount.java
+++ src/wordCount.java
@@ -5,7 +5,8 @@ public class wordCount {
     public static void main(String[] args) {
         System.out.println(wordCount(new String[]{"a", "b", "a", "c", "b"})); // {"a": 2, "b": 2, "c": 1}
         System.out.println(wordCount(new String[]{"c", "b", "a"}));  // {"a": 1, "b": 1, "c": 1}
-        System.out.println(wordCount(new String[]{"c", "c", "c", "c"}));  //
+        System.out.println(wordCount(new String[]{"c", "c", "c", "c"}));  //{"c": 4}
+
 
     }
 
