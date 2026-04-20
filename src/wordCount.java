d5df5891de3dd8e3dcdb2e57491bbf179d90acfd
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:26:57 +0400

    Task4:wordCount

--- src/wordCount.java
+++ src/wordCount.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 public class wordCount {
     public static void main(String[] args) {
         System.out.println(wordCount(new String[]{"a", "b", "a", "c", "b"})); // {"a": 2, "b": 2, "c": 1}
-        System.out.println(wordCount(new String[]{"c", "b", "a"}));  //
+        System.out.println(wordCount(new String[]{"c", "b", "a"}));  // {"a": 1, "b": 1, "c": 1}
         System.out.println(wordCount(new String[]{"c", "c", "c", "c"}));  //{"c": 4}
 
 
