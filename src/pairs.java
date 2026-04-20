6cec1ad06f0ceac1cbe3a982daf797d18666144c
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:19:52 +0400

    Task3:pairs

--- src/pairs.java
+++ src/pairs.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class pairs {
     public static void main (String[] args){
-        System.out.println();
+        System.out.println(pairs(new String[]{"code", "bug"}));
         System.out.println();
         System.out.println();
     }
