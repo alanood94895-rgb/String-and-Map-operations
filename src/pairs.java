179379f224b796f2880468af5e18f13e3e19c82c
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:19:07 +0400

    Task3:pairs

---
+++ src/pairs.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class pairs {
+    public static void main (String[] args){
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    public static Map<String, String> pairs (String[] String){
+        Map<String, String> result = new HashMap<>();
+
+         for (String str : String) {
+            String key = str.substring(0, 1); // first character
+            String value = str.substring(str.length() - 1); // last character
+            result.put(key, value);
+        }
+
+        return result;
+    }
+}
