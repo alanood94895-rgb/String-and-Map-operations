399592324ee428c6881910a2fde649c866bcd1cb
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:38:22 +0400

    Task5:firstChar

--- src/firstChar.java
+++ src/firstChar.java
@@ -5,7 +5,6 @@ public class firstChar {
     public static void main(String[] args) {
         System.out.println(firstChar(new String[]{"salt", "tea", "soda", "toast"}));
         System.out.println(firstChar(new String[]{"aa", "bb", "cc", "aAA", "cCC", "d"}));
-        System.out.println(firstChar(new String[]{}));
     }
 
     public static Map<String, String> firstChar(String[] strings) {
