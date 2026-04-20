6de54431557a59ee63e2240c928bcaee008a0069
CompanyProfile <alanood94895@gmail.com>
2026-04-20 11:09:38 +0400

    Task2:wordLen

---
+++ src/wordLen.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class wordLen {
+    public class void main (String[] args){
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    public static Map<String, Integer> wordLen (String[] String){
+        Map<String, Integer> result = new HashMap<>();
+
+        for (String str : String) {
+            result.put(str, str.length());
+        }
+        return result;
+
+    }
+}
